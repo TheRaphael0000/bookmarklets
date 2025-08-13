a9a00482a36431205f9add789d7ff7da27d8985c
Raphaël Margueron <raphael.margueron@gmail.com>
2025-08-13 20:01:45 +0200

    fix

--- prompt_to_wplace.js
+++ prompt_to_wplace.js
@@ -1,14 +1,14 @@
 javascript: (() => {
+    let location = prompt("Location:");
+    if (location == undefined || location == "") return;
     fetch(
-        `https://nominatim.openstreetmap.org/search?q=${prompt(
-            "Location:"
-        )}&format=jsonv2`
+        `https://nominatim.openstreetmap.org/search?q=${location}&format=jsonv2`
     )
         .then((r) => r.json())
         .then((json) => {
             let first = json[0];
             if (first == undefined) return;
-            var url = `https://wplace.live/?lat=${first.lat}&lng=${first.lon}&zoom=11`;
+            let url = `https://wplace.live/?lat=${first.lat}&lng=${first.lon}&zoom=11`;
             window.location.replace(url);
         });
 })();
